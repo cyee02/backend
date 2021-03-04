d9b274c9baf621e7e7e499b9f4c2c690a6fe348e
cyee02 <chiazy94@gmail.com>
2021-03-04 09:33:29 +0800

    added production build

---
+++ build/precache-manifest.6433733cb9f9449690fddf8621cd76a3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "993fcb65f8860afd7fc0",
+    "url": "/static/css/main.5af68444.chunk.css"
+  },
+  {
+    "revision": "993fcb65f8860afd7fc0",
+    "url": "/static/js/main.42d78946.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "225cfc2c5c5d7dd2c7ae",
+    "url": "/static/js/2.2be0eae5.chunk.js"
+  },
+  {
+    "revision": "952b65c2e8b6304610b48437863435e0",
+    "url": "/index.html"
+  }
+];
